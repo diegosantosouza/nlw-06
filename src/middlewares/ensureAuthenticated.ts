42cef752dca7523f7165a82437269624f3c81f33
diegosantosouza <diegosantos.s@hotmail.com>
2021-10-26 23:54:40 -0300

    ComplimentsService receive send

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+interface IPayload {
+    sub : string;
+}
+
+export function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
+
+    const authToken = request.headers.authorization;
+    if (!authToken) {
+        return response.status(401).end();
+    }
+
+    const [,token] = authToken.split(' ');
+
+    try {
+        const { sub } = verify(token, "9ccc5bdd91ec5a1bf7ff85a9e7e7eae6") as IPayload;
+        request.user_id = sub;
+        return next();
+    } catch (err) {
+        return response.status(401).end();
+    } 
+}
